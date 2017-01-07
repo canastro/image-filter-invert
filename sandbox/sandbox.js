79936ff7f5cfb0ff4aee8231723ae4984e34a435
canastro <ricardocanastro@gmail.com>
2017-01-07 23:31:01 +0000

    update image-filter-core dependency

--- sandbox/sandbox.js
+++ sandbox/sandbox.js
@@ -1,43 +1,29 @@
-import imageInvert from '../src/index';
+var imageFilterCore = require('image-filter-core');
+var imageInvert = require('../src/index');
 
-function applyResults(selector, src) {
-    var target;
-    var image;
-
-    target = document.querySelectorAll(selector)[0];
-
-    image = document.createElement('img');
-    image.setAttribute('src', src);
+function applyResults(selector, canvas, context, src) {
+    var target = document.querySelectorAll(selector)[0];
+    var image = document.createElement('img');
+    image.setAttribute('src', imageFilterCore.convertImageDataToCanvasURL(src));
     target.appendChild(image);
 }
 
 window.onload = function () {
-    const img = new Image;
-
-    img.onload = () => {
-        const canvas = document.createElement('canvas');
+    var img = new Image;
+    img.onload = function () {
+        var canvas = document.createElement('canvas');
         canvas.width = img.width;
         canvas.height = img.height;
-        const context = canvas.getContext('2d');
+        var context = canvas.getContext('2d');
         context.drawImage(img, 0, 0);
 
-        const data = context.getImageData(0, 0, img.width, img.height);
+        var data = context.getImageData(0, 0, img.width, img.height);
 
-        imageInvert({
-            data: data,
-            adjustment: 30,
-            asDataURL: true
-        }).then((results) => {
-            applyResults('#target-1', results);
-        });
-
-        imageInvert({
-            data: data,
-            adjustment: 70,
-            asDataURL: true
-        }).then((results) => {
-            applyResults('#target-2', results);
-        });
+        imageInvert(data)
+            .then(function (results) {
+                applyResults('#target-1', canvas, context, results);
+            });
     };
+
     img.src = 'dummy.jpg';
 };
